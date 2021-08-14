5d2fe1d7fa692bc64338e66c34e9ba87f8ce5d37
Tora <waltva0@gmail.com>
2021-08-14 18:31:47 -0500

    Removed superfluous code. Updated .README

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,6 @@ var uppercaseCheck;
 var numericCheck;
 var specialCheck;
 var userChoice;
-var noChoice;
 
 // Set length of password anywhere between 8 and 128 characters. Functions prevent invalid input.
 function createPass() {
@@ -23,37 +22,35 @@ function createPass() {
     alert("You must enter a valid number between 8 and 128.");
   } else if (start < 8 || start > 128) {
     start = parseInt(alert("You must enter a valid number between 8 and 128."));
-  }
-    else {
-// Boolean that stores user choices.
-lowercaseCheck = confirm("Would you like to use lowercase letters?");
-uppercaseCheck = confirm("Would you like to use uppercase letters?");
-numericCheck = confirm("Would you like to use numbers?");
-specialCheck = confirm("Would you like to use special characters?");
+  } else {
+    // Boolean that stores user choices.
+    lowercaseCheck = confirm("Would you like to use lowercase letters?");
+    uppercaseCheck = confirm("Would you like to use uppercase letters?");
+    numericCheck = confirm("Would you like to use numbers?");
+    specialCheck = confirm("Would you like to use special characters?");
 
-if (lowercaseCheck) {
-  userChoice += lowercaseChar;
-}
-if (uppercaseCheck) {
-  userChoice += uppercaseChar;
-}
-if (numericCheck) {
-  userChoice += numericChar;
-}
-if (specialCheck) {
-  userChoice += specialChar;
-}
-
-// Generates random password based on user choices.
-for (var i = 0; i < start; i++) {
-  password += userChoice.charAt(
-    Math.floor(Math.random() * userChoice.length)
-  );
-}
-return password;
-}
+    if (lowercaseCheck) {
+      userChoice += lowercaseChar;
     }
-  
+    if (uppercaseCheck) {
+      userChoice += uppercaseChar;
+    }
+    if (numericCheck) {
+      userChoice += numericChar;
+    }
+    if (specialCheck) {
+      userChoice += specialChar;
+    }
+
+    // Generates random password based on user choices.
+    for (var i = 0; i < start; i++) {
+      password += userChoice.charAt(
+        Math.floor(Math.random() * userChoice.length)
+      );
+    }
+    return password;
+  }
+}
 
 // Write password to the #password input.
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PasswordGenerator
+
+<img src="https://i.imgur.com/U6IzmQA.png">
+
+# Generates a password anywhere between 8-128 characters. Valid characters are:
+ abcdefghijklmnopqrstuvwxyz
+ ABCDEFGHIJKLMNOPQRSTUVWXYZ
+ 0123456789
+ !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+# Invalid characters result in: 
+In the event an invalid input (less than 8, more than 128, not a number) is entered for the purposes of determining password length, the user choice prompts will be ignored and the password will return as undefined.
+
+# Link to deployed application
+<a href=""></a>
