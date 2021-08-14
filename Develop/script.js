8b94fdff2628ffcfb70d7661b45267d960c3e17d
Tora <waltva0@gmail.com>
2021-08-14 18:17:56 -0500

    Edited syntax to remove errors. Proper capitalization on Math, userChoice, added Else statement so there are no userChoice prompts in the event an invalid character is added.

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,67 @@
-// Assignment Code
+// Assignment Code.
 var generateBtn = document.querySelector("#generate");
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChar = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var password = "";
+var passwordLength;
+var lowercaseCheck;
+var uppercaseCheck;
+var numericCheck;
+var specialCheck;
+var userChoice;
+var noChoice;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Set length of password anywhere between 8 and 128 characters. Functions prevent invalid input.
+function createPass() {
+  var start = prompt(
+    "Set length of password anywhere between 8 and 128 characters."
+  );
 
-  passwordText.value = password;
+  if (!start) {
+    alert("You must enter a valid number between 8 and 128.");
+  } else if (start < 8 || start > 128) {
+    start = parseInt(alert("You must enter a valid number between 8 and 128."));
+  }
+    else {
+// Boolean that stores user choices.
+lowercaseCheck = confirm("Would you like to use lowercase letters?");
+uppercaseCheck = confirm("Would you like to use uppercase letters?");
+numericCheck = confirm("Would you like to use numbers?");
+specialCheck = confirm("Would you like to use special characters?");
 
+if (lowercaseCheck) {
+  userChoice += lowercaseChar;
+}
+if (uppercaseCheck) {
+  userChoice += uppercaseChar;
+}
+if (numericCheck) {
+  userChoice += numericChar;
+}
+if (specialCheck) {
+  userChoice += specialChar;
 }
 
-// Add event listener to generate button
+// Generates random password based on user choices.
+for (var i = 0; i < start; i++) {
+  password += userChoice.charAt(
+    Math.floor(Math.random() * userChoice.length)
+  );
+}
+return password;
+}
+    }
+  
+
+// Write password to the #password input.
+function writePassword() {
+  var password = "";
+  var password = createPass();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button.
 generateBtn.addEventListener("click", writePassword);
